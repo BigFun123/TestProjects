edb96500f4923ac430903c57878307e134396c48
karl <github@bigfun.co.za>
2025-12-21 19:09:59 +0200

    Revert "tailwind, next"
    This reverts commit 39411ab12ee32f5bcc29ab5116cd1c98e623906e.

--- AWS/scripts/1-run-local.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Run the .NET application locally (without Docker)
-echo Starting .NET application...
-cd HelloWorldApp
-dotnet run
--- AWS/scripts/3-docker-stop.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Stop and remove local Docker container
-echo Stopping container...
-docker stop helloworld-container
-
-echo Removing container...
-docker rm helloworld-container
-
-echo Done!
